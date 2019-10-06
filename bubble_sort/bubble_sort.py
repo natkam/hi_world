2df3e15227ccba1df24713e11d899cf90142a134
natkam <natka_m@o2.pl>
2019-10-06 03:29:02 +0200

    Improve bubble sort slightly (mainly formatting)

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,20 +1,24 @@
+import random
 import time
-start_clock = time.clock()
+
 
 def bubble_sort(list_to_sort):
-    length = len(list_to_sort)
-    for j in range(1, length):
+    for j in range(1, len(list_to_sort)):
         bubble_up(list_to_sort, j)
 
+
 def bubble_up(list_to_sort, j):
-    length = len(list_to_sort)
-    for i in range(length - j):
+    for i in range(len(list_to_sort) - j):
         if list_to_sort[i] > list_to_sort[i + 1]:
             list_to_sort[i], list_to_sort[i + 1] = list_to_sort[i + 1], list_to_sort[i]
 
-list_to_sort = [46, 69, 8, 26, 14, 78, 12, 24]
-list_to_sort_worst_case = list(range(10000, 0, -1))
 
-bubble_sort(list_to_sort_worst_case)
+list_to_sort = [random.randint(0, 10000) for _ in range(5000)]
+list_to_sort_worst_case = list(range(5000, 0, -1))
 
-print('--- %s seconds ---' % (time.clock() - start_clock))
+
+if __name__ == "__main__":
+    start_clock = time.perf_counter()
+    bubble_sort(list_to_sort)
+    bubble_sort(list_to_sort_worst_case)
+    print("--- %s seconds ---" % (time.perf_counter() - start_clock))
